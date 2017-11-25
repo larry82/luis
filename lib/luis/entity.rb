2bf74972cf77f99ec42b8f193e3929feb23f5127
涂立青 <larry@tuliqingde-MacBook-Pro.local>
2017-11-25 15:03:43 +0800

    fix resilution

--- lib/luis/entity.rb
+++ lib/luis/entity.rb
@@ -1,6 +1,19 @@
 module Luis
   # Luis entity class
   class Entity < Base
-    attr_accessor :entity, :type, :start_index, :end_index, :score, :resolution
+    attr_accessor :entity, :type, :start_index, :end_index, :score
+
+    def resolution
+      if @resolution.present?
+        begin
+          @resolution["values"][0]
+        rescue
+          nil
+        end
+      else
+        nil
+      end
+    end
+
   end
 end
