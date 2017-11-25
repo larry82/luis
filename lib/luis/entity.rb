03ffc86e1803afafb6598b1e16105268e4495f95
涂立青 <larry@tuliqingde-MacBook-Pro.local>
2017-11-25 14:58:50 +0800

    add resolution

--- lib/luis/entity.rb
+++ lib/luis/entity.rb
@@ -1,6 +1,6 @@
 module Luis
   # Luis entity class
   class Entity < Base
-    attr_accessor :entity, :type, :start_index, :end_index, :score
+    attr_accessor :entity, :type, :start_index, :end_index, :score, :resolution
   end
 end
